fc1799b4038fdd41134d2edd5bbf12009afa5228
Arvind Kumar <arvind@inimisttech.com>
2022-11-09 10:41:37 +0530

    first commit

---
+++ src/TangoCardResponse.php
@@ -0,0 +1,23 @@
+<?php
+namespace Buildrr\TangoRaasApi;
+
+class TangoCardResponse {
+
+  public $status = null;
+
+  public $data = null;
+
+  public function __construct($status = true, $data) {
+    $this->status = $status;
+    $this->data = json_decode($data);
+  }
+
+  public function getStatus(){
+   return $this->status;
+  }
+
+  public function getData(){
+   return $this->data;
+  }
+}
+?>
